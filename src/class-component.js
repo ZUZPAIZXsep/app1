24dd4645471c4791d2abd17962cf855c08f2a938
ZUZPAIZXsep <s6303051613092@email.kmutnb.ac.th>
2023-08-11 15:20:07 +0700

    Update Class Component
    Update Class Component

--- src/class-component.js
+++ src/class-component.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 export default class Calendar extends Component{
     getDate(){
         const dayNames = ['อาทิตย์','จันทร์','อังคาร','พุธ','พฤหัสบดี','ศุกร์','เสาร์']
-        
+
         const monthNames=['มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม',
                         'กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม']
         const date = new Date()
@@ -12,7 +12,7 @@ export default class Calendar extends Component{
         const day = date.getDate()
         const month = monthNames[date.getMonth()]
         const year = date.getFullYear()+ 543
-        return 'วัน${weekDay} ที่ ${day} ${month} ${year}'
+        return `วัน${weekDay} ที่ ${day} ${month} ${year}`
     }
 
     render(){
