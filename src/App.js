3e82e8149a4170f133433fb4efdd0cf277012894
ZUZPAIZXsep <s6303051613092@email.kmutnb.ac.th>
2023-08-18 14:10:57 +0700

    Add banner.js
    add banner

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { Header,Content,Footer } from './func-components';
-import Calendar from './class-component';
+import Banner from './banner'
 
-function App() {
-  return (
-    <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  )
+
+export default function App() {
+  return <Banner/>
 }
 
-export default App
+
