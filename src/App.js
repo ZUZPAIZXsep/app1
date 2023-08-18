dfcb3ade7d30fcfbb954f76a109ef6a6562240ee
ZUZPAIZXsep <s6303051613092@email.kmutnb.ac.th>
2023-08-18 15:20:32 +0700

    add event-data
    add event-data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Banner from './banner'
-import { Calculator } from './calculator';
+import {EventData2} from './event-data';
 
 
 export default function App() {
   
-  return <Calculator/>
+  return <EventData2/>
 }
 
 
