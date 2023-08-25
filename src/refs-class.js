cc3d74e4907987940bc23a5614bac2aabb53d28d
ZUZPAIZXsep <s6303051613092@email.kmutnb.ac.th>
2023-08-25 14:18:37 +0700

    Add refs-func

--- src/refs-class.js
+++ src/refs-class.js
@@ -1,35 +1,31 @@
-import React,{ createRef } from "react";
-
-export default class RefsClass extends React.Component {
-    num1 = createRef()
-    num2 = createRef()
-    operator = React.createRef()
-    result = React.createRef()
-
-    calculate = (ev) => {
-        let op = ev.target.innerText
-        let n1 = parseInt(this.num1.current.innerText)
-        let n2 = parseInt(this.num2.current.innerText)
-        let r = eval(`${n1} ${op} ${n2}`)
-        this.result.current.innerText = r
-        this.operator.current.innerText = op
-    }
-
-    render() {
-        return (
-            <div style={{ textAlign: 'center', marginTop: 20, fontSize: 18 }}>
-                <span ref={this.num1}>15</span>&nbsp;
-                <span ref={this.operator}>?</span>&nbsp;
-                <span ref={this.num2}>5</span>&nbsp;
-                <span>=</span>&nbsp;
-                <span ref={this.result}>?</span>
-                <div>
-                    <button onClick={this.calculate}>+</button>&nbsp;
-                    <button onClick={this.calculate}>-</button>&nbsp;
-                    <button onClick={this.calculate}>*</button>&nbsp;
-                    <button onClick={this.calculate}>/</button>
-                </div>
-            </div>
-        )
-    }
+import React, { useRef } from 'react'
+export default function RefsFunc() {
+const num1 = useRef()
+const num2 = useRef()
+const operator = React.useRef()
+const result = React.useRef()
+const calculate = (ev) => {
+let op = ev.target.innerText
+let n1 = parseInt(num1.current.innerText)
+let n2 = parseInt(num2.current.innerText)
+let r = eval(`${n1} ${op} ${n2}`)
+result.current.innerText = r
+operator.current.innerText = op
 }
+
+return (
+    <div style={{ textAlign: 'center', marginTop: 20, fontSize: 18 }}>
+    <span ref={num1}>15</span>&nbsp;
+    <span ref={operator}>?</span>&nbsp;
+    <span ref={num2}>5</span>&nbsp;
+    <span>=</span>&nbsp;
+    <span ref={result}>?</span>
+    <div>
+    <button onClick={calculate}>+</button>&nbsp;
+    <button onClick={calculate}>-</button>&nbsp;
+    <button onClick={calculate}>*</button>&nbsp;
+    <button onClick={calculate}>/</button>
+    </div>
+    </div>
+    )
+    }
