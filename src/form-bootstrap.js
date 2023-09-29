2f75732df26cc5f2ed214cc625d276ea4a465be9
ZUZPAIZXsep <s6303051613092@email.kmutnb.ac.th>
2023-09-29 14:49:33 +0700

    add form-text.js and App.js
    add form-text.js and App.js

---
+++ src/form-bootstrap.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function FormBootstrap(){
+    return(
+        <div className="mt-3 mx-auto p-3" style={{width:'400px',background:'#cee'}}></div>
+
+    )
+}
